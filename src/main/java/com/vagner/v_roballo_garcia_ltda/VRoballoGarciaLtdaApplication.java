7b2e71df07fe34e2d93871b9b77f612ab23ee7cb
Vagner Roballo <vagnerrgsb@gmail.com>
2025-11-23 21:44:53 -0300

    criação spring para v_roballo_garcia_ltda

---
+++ src/main/java/com/vagner/v_roballo_garcia_ltda/VRoballoGarciaLtdaApplication.java
@@ -0,0 +1,13 @@
+package com.vagner.v_roballo_garcia_ltda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VRoballoGarciaLtdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VRoballoGarciaLtdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vagner/v_roballo_garcia_ltda/VRoballoGarciaLtdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagner.v_roballo_garcia_ltda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VRoballoGarciaLtdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
